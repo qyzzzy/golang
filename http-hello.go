813d28ec4b8df42d12a62aa4ae9c953fcc79a968
Administrator <admin@example.com>
2024-02-27 18:54:49 +0800

    first commit!

--- http-hello.go
+++ http-hello.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world!")
+	fmt.Fprintf(w, "你好, world!")
 }
 func main() {
 	http.HandleFunc("/", handler)
